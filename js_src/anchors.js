b6d6445a12a76c69839771ece69aaf1fb6639df1
Elliott Williams <e@elliottwillia.ms>
2016-11-23 10:11:56 -0500

    changing the way virtual tour is embedded, added back to top link

--- js_src/anchors.js
+++ js_src/anchors.js
@@ -19,7 +19,7 @@
 
   // Embeds the virtual tour and scroll to it.
   function showVirtualTour(animated) {
-    window.manageSection('virtualTour');
+    $('#virtualTour').addClass('expanded').removeClass('collapsed');
     $('body').scrollTo($('#virtualTour'), {
       duration: animated ? 500 : 0,
       onAfter: function () {
